d7950b297e487f4c83dd78ee2dd36eb637548bf2
Rodolpho Gouveia <rodolphogouveiageo@gmail.com>
2024-02-22 22:43:22 -0300

    Relógio digital e data atual

--- js/script.js
+++ js/script.js
@@ -1,46 +1,54 @@
-const horas = document.getElementById('horas');
-const minutos = document.getElementById('minutos');
-const segundos = document.getElementById('segundos');
+setInterval(function() {
 
+  const hours = document.getElementById('hours');
+  const minutes = document.getElementById('minutes');
+  const seconds = document.getElementById('seconds');
 
-const relogio = setInterval(function time() {
-    let dateToday = new Date();
-    let hr = dateToday.getHours(); /*Atribuindo a hora exata a variável hr*/
-    let min = dateToday.getMinutes(); /*Atribuindo o minuto exato a variável min*/
-    let seg = dateToday.getSeconds(); /*Atribuindo o segundo exato a variável seg*/
-    let saud = document.getElementById('saudacao');
+  let today = new Date();
+  let hr = today.getHours(); /*Atribuindo a hora exata a variável hr*/
+  let min = today.getMinutes(); /*Atribuindo o minuto exato a variável min*/
+  let sec = today.getSeconds(); /*Atribuindo o segundo exato a variável seg*/
+  let salt = document.getElementById('salutation');
 
-    if (hr <10) hr = '0' + hr;
-    if (min <10) min = '0' + min;
-    if (seg <10) seg = '0' + seg;
-  
-    if (hr < 12) {
-        saud = "Bom dia";
-      } else if (hr < 18) {
-        saud = "Boa tarde";
-      } else {
-        saud = "Boa noite";
-      }
-  
-    horas.textContent = hr; /*textContent altera o que está escrito no span pra mostrar o que está dentro da variável hr*/
-    minutos.textContent = min;
-    segundos.textContent = seg;
-    saudacao.textContent = saud;
+  if(hr < 10){
+    hr = "0" + hr;
+  }
+  if(min < 10){
+    min = "0" + min;
+  }
+  if(sec < 10){
+    sec = "0" + sec;
+  }
+
+  if (hr < 12) {
+    salt = "Bom dia";
+  } else if (hr < 18) {
+    salt = "Boa tarde";
+  } else {
+    salt = "Boa noite";
+  }
+
+  hours.textContent = hr;
+  minutes.textContent = min;
+  seconds.textContent = sec;
+  salutation.textContent = salt;
 
 })
 
-const calendario = setInterval(function calendario() {
+setInterval(function calendario() {
+
     dayName = new Array ("domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado");
     monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto", "Outubro", "Novembro", "Dezembro");
-    let calendarToday = new Date();
-    let sem = dayName[calendarToday.getDay()];
-    let di = calendarToday.getDate();
-    let me = monName[calendarToday.getMonth()];
-    let an = calendarToday.getFullYear();
     
-    semana.textContent = sem;
-    dia.textContent = di;
-    mes.textContent = me;
-    ano.textContent = an;
-
+    let today = new Date();
+    let we = dayName[today.getDay()];
+    let da = today.getDate();
+    let mo = monName[today.getMonth()];
+    let ye = today.getFullYear();
+    
+    week.textContent = we;
+    day.textContent = da;
+    month.textContent = mo;
+    year.textContent = ye;
+    
 })
